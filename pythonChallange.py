3c088adeb79a1004ef1bd36076f4519b2dc696b0
msalgado85 <156229265+msalgado85@users.noreply.github.com>
2024-09-13 12:38:05 -0500

    Add files via upload

--- pythonChallange.py
+++ pythonChallange.py
@@ -1,4 +1,4 @@
 name = input("What is your name? ")
 monthSales = float(input("How much did you make in sales this month? "))
-commission = (float(monthSales) * 0.13)
+commission = monthSales* 0.13
 print("Welcome, " + name + ". You've made a total of " + str(round(commission)) + " dollars! ")
